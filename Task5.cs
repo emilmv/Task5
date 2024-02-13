872ac9fa2bb22cb9b47c305a94ce9c08b31319b9
emilmv <emilnm@code.edu.az>
2024-02-14 01:39:26 +0400

    Task5

---
+++ Task5.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region Task5.1
+//int n = 50;
+//int m = 100;
+//int result = 0;
+//if (n < m)
+//{
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            result++;
+//        }
+//    }
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("Düzgün ədəd daxil edin");
+//}
+#endregion
+#region Task5.2
+//int n = 11;
+//if (n==2||n==3)
+//{
+//    Console.WriteLine("Sadədir");
+//}
+//else if(n%2==0||n%3==0)
+//{
+//    Console.WriteLine("Mürəkkəbdir");
+//}
+//else
+//{
+//    Console.WriteLine("Sadədir");
+//}
+#endregion
+#region Task5.3
+//int a = 20;
+//int b = 5;
+//if (a % b == 0)
+//{
+//    Console.WriteLine("Tam bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+#endregion
+#region Task5.4
+//int x = 20;
+//int y = 40;
+//int z = 50;
+//int result = 0;
+//if (x > y)
+//{
+//    result += x;
+//}
+//else
+//{
+//    result += y;
+//}
+//if (y > z)
+//{
+//    result += y;
+//}
+//else
+//{
+//    result += z;
+//}
+//Console.WriteLine(result);
+#endregion
